bb03dc38aac9b3afa307ba4aaab85f2eeae6044e
Adi <264791717+suryadi346-star@users.noreply.github.com>
2026-03-04 03:58:09 +0700

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,8 @@
+-- ============================================================
+--  Entry Point — Neovim Config
+--  Author : suryadiarsyil-ops
+--  Repo   : https://github.com/suryadiarsyil-ops
+-- ============================================================
+
+require("core.setup")   -- Plugin manager (lazy.nvim) + semua plugin
+require("core.keymaps") -- Clipboard Termux + keymaps global
